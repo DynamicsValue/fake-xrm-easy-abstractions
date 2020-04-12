fee78f885580141fa7ac33e21bf324c88d723adf
jordimontana82 <jordi.montana@gmail.com>
2020-04-13 00:26:53 +0200

    Updated nuget package metadata

--- build-push.ps1
+++ build-push.ps1
@@ -6,6 +6,6 @@ Write-Host "Packing assembly..."
 dotnet pack src/FakeXrmEasy.Abstractions/FakeXrmEasy.Abstractions.csproj
 
 Write-Host "Pushing FakeXrmEasy.Abstractions to local folder..."
-dotnet nuget push /home/jordi/code/DynamicsValue/fake-xrm-easy-abstractions/src/FakeXrmEasy.Abstractions/bin/Debug/FakeXrmEasy.Abstractions.1.0.0.nupkg -s local-packages
+dotnet nuget push ./src/FakeXrmEasy.Abstractions/bin/Debug/FakeXrmEasy.Abstractions.2.0.0.1-Alpha.nupkg -s local-packages
 
 Write-Host "Succeeded :)"
