f72b140da0b66b572715f05671b4adcc0f24d680
jordimontana82 <jordi.montana@gmail.com>
2020-04-30 04:42:21 +0200

    Add the possibility to extend the context with any custom property via interface methods, without overpopulating the context with too many properties

--- src/FakeXrmEasy.Abstractions/IXrmBaseContext.cs
+++ src/FakeXrmEasy.Abstractions/IXrmBaseContext.cs
@@ -9,5 +9,14 @@ namespace FakeXrmEasy.Abstractions
         /// </summary>
         /// <returns></returns>
         IOrganizationService GetOrganizationService();
+
+        //Set a value of a specific custom property of a given type
+        void SetProperty<T>(T property);
+
+        /// <summary>
+        /// Returns a custom property
+        /// </summary>
+        /// <returns>The property requested or exception if property wasn't found</returns>
+        T GetProperty<T>();
     }
 }
