03d6667147a04d280c5e588736806c2ac94a7afb
jordimontana82 <jordi.montana@gmail.com>
2020-05-21 01:31:23 +0200

    Splitted pack & push into 2 steps before and after quality gate

--- push.ps1
+++ push.ps1
@@ -1,45 +1,17 @@
 param (
-    [string]$packageSource = "local-packages",
-    [string]$versionSuffix = ""
+    [string]$packageSource = "local-packages"
  )
 
 $project = "FakeXrmEasy.Abstractions"
 
-Write-Host "Running with packageSource '$($packageSource)' and versionSuffix '$($versionSuffix)'..."
+Write-Host "Running with packageSource '$($packageSource)'..."
 
 $tempNupkgFolder = './nupkgs'
-$localPackagesFolder = '../' + $packageSource
-
-Write-Host "Checking if temp nupkgs folder '$($tempNupkgFolder)' exists..."
-
-$tempNupkgFolderExists = Test-Path $tempNupkgFolder -PathType Container
-
-if(!($tempNupkgFolderExists)) 
-{
-    New-Item $tempNupkgFolder -ItemType Directory
-}
-
-Write-Host "Deleting temporary nupkgs..."
-Get-ChildItem -Path $tempNupkgFolder -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
 
 if($packageSource -eq "local-packages") {
     Write-Host "Deleting previous pushed version '$($localPackagesFolder)'..."
     $projectFilePattern = $project + ".*"
     Get-ChildItem -Path $localPackagesFolder -Include $projectFilePattern -File -Recurse | ForEach-Object { $_.Delete()}
-}
-
-
-Write-Host "Packing assembly..."
-if($versionSuffix -eq "") 
-{
-    dotnet pack -o $tempNupkgFolder src/$project/$project.csproj
-}
-else {
-    dotnet pack -o $tempNupkgFolder src/$project/$project.csproj --version-suffix $versionSuffix
-}
-
-if(!($LASTEXITCODE -eq 0)) {
-    throw "Error when packing the assembly"
 }
 
 Write-Host "Pushing '$($project)' to source '$($packageSource)'..."
