1dd6938ddfc19b4ae1ddcb02b8e9629501cc0782
Jordi <jordi.montana@gmail.com>
2020-12-27 16:47:32 -0800

    Added multiple configurations for abstractions

--- src/FakeXrmEasy.Abstractions/Plugins/IXrmFakedPluginContextProperties.cs
+++ src/FakeXrmEasy.Abstractions/Plugins/IXrmFakedPluginContextProperties.cs
@@ -12,10 +12,12 @@ namespace FakeXrmEasy.Abstractions.Plugins
 
         IOrganizationServiceFactory OrganizationServiceFactory { get; }
 
+#if FAKE_XRM_EASY_9
         IEntityDataSourceRetrieverService EntityDataSourceRetrieverService { get; }
 
         Entity EntityDataSourceRetriever { get; set; }
-        
+#endif
+
         IServiceProvider GetServiceProvider(XrmFakedPluginExecutionContext plugCtx);
 
     }
