2a069670a3a3dcd47806906130e57382ef2094dc
jordimontana82 <jordi.montana@gmail.com>
2021-04-24 20:34:39 +0200

    Updated scripts to build on linux

--- pack.ps1
+++ pack.ps1
@@ -19,11 +19,20 @@ if(!($tempNupkgFolderExists))
 Write-Host "Deleting temporary nupkgs..."
 Get-ChildItem -Path $tempNupkgFolder -Include *.nupkg -File -Recurse | ForEach-Object { $_.Delete()}
 
-./pack-configuration.ps1 -configuration "FAKE_XRM_EASY" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
-./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
-./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
-./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
-./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
-./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+if($targetFrameworks -eq "netcoreapp3.1")
+{
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+}   
+else 
+{
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_2013" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_2015" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_2016" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_365" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+    ./pack-configuration.ps1 -configuration "FAKE_XRM_EASY_9" -versionSuffix $versionSuffix -targetFrameworks $targetFrameworks
+}
+
 
 Write-Host "Pack All Configurations Succeeded  :)" -ForegroundColor Green
