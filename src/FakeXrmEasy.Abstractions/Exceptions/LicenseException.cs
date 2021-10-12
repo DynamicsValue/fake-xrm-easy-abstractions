a95847a87c3e9d044591646d9f0efa0e6d6c620f
Jordi <jordi.montana@gmail.com>
2021-10-12 21:53:19 +0200

    Remove serialization settings after deactivating Sonar rule

--- src/FakeXrmEasy.Abstractions/Exceptions/LicenseException.cs
+++ src/FakeXrmEasy.Abstractions/Exceptions/LicenseException.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace FakeXrmEasy.Abstractions.Exceptions
 {
-    [Serializable]
     public class LicenseException : Exception
     {
         public LicenseException(string message) : base(message)
-        {
-
-        }
-
-        protected LicenseException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
 
         }
