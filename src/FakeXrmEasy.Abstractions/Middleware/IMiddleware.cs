fe70b7f9a255aa5a8b4824e39e524d4656ab7daf
Jordi <jordi.montana@gmail.com>
2021-10-12 22:32:38 +0200

    Added SetLicense method in MiddleBuilder

--- src/FakeXrmEasy.Abstractions/Middleware/IMiddleware.cs
+++ src/FakeXrmEasy.Abstractions/Middleware/IMiddleware.cs
@@ -1,9 +1,11 @@
+using FakeXrmEasy.Abstractions.Enums;
 using System;
 
 namespace FakeXrmEasy.Abstractions.Middleware
 {
     public interface IMiddlewareBuilder
     {
+        IMiddlewareBuilder SetLicense(FakeXrmEasyLicense license);
         IMiddlewareBuilder Add(Action<IXrmFakedContext> context);
         IMiddlewareBuilder Use(Func<OrganizationRequestDelegate, OrganizationRequestDelegate> middleware);
 
