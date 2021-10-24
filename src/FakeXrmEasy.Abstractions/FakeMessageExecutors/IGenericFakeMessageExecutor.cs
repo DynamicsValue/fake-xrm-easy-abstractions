b47d3a95dc8845d99fb264b05011e76827a09143
jordimontana82 <jordi.montana@gmail.com>
2021-10-24 21:49:09 +0200

    Added generic message executor interface

---
+++ src/FakeXrmEasy.Abstractions/FakeMessageExecutors/IGenericFakeMessageExecutor.cs
@@ -0,0 +1,14 @@
+namespace FakeXrmEasy.Abstractions.FakeMessageExecutors
+{
+    /// <summary>
+    /// An interface that adds an additional method in order to discover generic requests by name
+    /// </summary>
+    public interface IGenericFakeMessageExecutor : IFakeMessageExecutor
+    {
+        /// <summary>
+        /// The OrganizationRequest's RequestName property
+        /// </summary>
+        /// <returns></returns>
+        string GetRequestName();
+    }
+}
