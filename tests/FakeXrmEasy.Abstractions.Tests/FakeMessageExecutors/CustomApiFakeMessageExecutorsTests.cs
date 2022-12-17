a9b2d4b3b3c8524caf4f88f40adc10ae58e347e8
Jordi <jordi.montana@gmail.com>
2022-12-17 22:10:22 +0100

    Increase code coverage

---
+++ tests/FakeXrmEasy.Abstractions.Tests/FakeMessageExecutors/CustomApiFakeMessageExecutorsTests.cs
@@ -0,0 +1,24 @@
+﻿using FakeXrmEasy.Abstractions.FakeMessageExecutors;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FakeXrmEasy.Abstractions.Tests.FakeMessageExecutors
+{
+    public class CustomApiFakeMessageExecutorsTests
+    {
+        [Fact]
+        public void Should_create_empty_collection()
+        {
+            var executors = new CustomApiFakeMessageExecutors();
+            Assert.NotNull(executors);
+        }
+
+        [Fact]
+        public void Should_create_message_executors_instance()
+        {
+            var other = new Dictionary<string, IBaseCustomApiFakeMessageExecutor>();
+            var executors = new CustomApiFakeMessageExecutors(other);
+            Assert.NotNull(executors);
+        }
+    }
+}
