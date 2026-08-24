9ec7e7460a0bf9adafc7e0bfe39242b9cd66bcd3
Jordi <jordi.montana@gmail.com>
2026-08-24 23:58:51 +0200

    Adds  a fix for #273

--- build.ps1
+++ build.ps1
@@ -57,15 +57,21 @@ if(!($LASTEXITCODE -eq 0)) {
     throw "Error during build step"
 }
 Write-Host " -> Testing: configuration='$($configuration)', targetFramework='$($targetFrameworks)' PackTests=$($packTests)" -ForegroundColor Yellow
-if($targetFrameworks -eq "all")
-{
-    dotnet test --configuration $configuration --no-build --verbosity normal /p:PackTests=$packTests --collect:"XPlat code coverage" --settings tests/.runsettings --results-directory ./coverage
+if($IsLinux) {
+    ./packages/xunit.runner.console/2.4.1/tools/net462/xunit.console.exe ./tests/FakeXrmEasy.Abstractions.Tests/bin/$configuration/**/FakeXrmEasy.Abstractions.Tests.dll
+}
+else {
+    if($targetFrameworks -eq "all")
+    {
+        dotnet test --configuration $configuration --no-build --verbosity normal /p:PackTests=$packTests --collect:"XPlat code coverage" --settings tests/.runsettings --results-directory ./coverage
 
+    }
+    else 
+    {
+        dotnet test --configuration $configuration --no-build --framework $targetFrameworks --verbosity normal /p:PackTests=$packTests --collect:"XPlat code coverage" --settings tests/.runsettings --results-directory ./coverage
+    }
 }
-else 
-{
-    dotnet test --configuration $configuration --no-build --framework $targetFrameworks --verbosity normal /p:PackTests=$packTests --collect:"XPlat code coverage" --settings tests/.runsettings --results-directory ./coverage
-}
+
 
 if(!($LASTEXITCODE -eq 0)) {
     throw "Error during test step"
